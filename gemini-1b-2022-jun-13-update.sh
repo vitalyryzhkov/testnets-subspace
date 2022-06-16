10f96d01a65335e62600da9105258b6581b3fcbf
vitalyryzhkov <pentagon0555@gmail.com>
2022-06-16 16:27:35 +0300

    Update gemini-1b-2022-jun-13-update.sh

--- gemini-1b-2022-jun-13-update.sh
+++ gemini-1b-2022-jun-13-update.sh
@@ -78,7 +78,7 @@ function eof_docker_compose {
         "--node-rpc-url", "ws://node:9944",
         "--ws-server-listen-addr", "0.0.0.0:9955",
         "--reward-address", "$WALLET_ADDRESS",
-        "--plot-size", "500G"
+        "--plot-size", "800G"
       ]
   volumes:
     node-data:
